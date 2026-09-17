68607ee02e4080ef8167df779a6b04bad56fda77
404-DevNotFound <dante.engineer@gmail.com>
2026-09-17 16:10:06 +0800

    baiki BIO

--- src/content.js
+++ src/content.js
@@ -2,8 +2,7 @@ export const profile = {
   name: 'whotao',
   // Ganti teks perkenalan di bawah banner hello di sini.
   bio: [
-    'Ai Enthusiastic.',
-    'I like to explore the world of AI and its applications. Besides that, I also enjoy writing. You can visit my community secarikkertas if you want ^^',
+    "Hello! I am deeply passionate about exploring the ever-evolving world of Artificial Intelligence and its limitless applications. I enjoy analyzing how this technology shapes our future. Alongside my journey in tech, I have a profound love for the art of writing, which serves as my primary creative outlet. I believe that technology and storytelling can go hand in hand. If you share these interests, I would be thrilled to welcome you to my community, secarikkertas. Please feel free to visit, connect, and let's explore these fascinating worlds together"
   ],
   community: 'https://secarikkertas.id',
   linkedin: 'https://www.linkedin.com/in/richarddante18/',
